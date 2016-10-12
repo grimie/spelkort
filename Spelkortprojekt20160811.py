31d4f3014246a3fac497c7cc919273378a746d0e
Grim Mostrom <grimmostrom@ext153-46.eduroam.uu.se>
2016-10-12 16:14:02 +0200

    alla hander klara

--- Spelkortprojekt20160811.py
+++ Spelkortprojekt20160811.py
@@ -31,31 +31,12 @@ print hand_farg
 hand_siffror = []
 for a in hand:
     hand_siffror.append(a[1])
-hand_siffror = [1, 2, 3, 4, 5]
+hand_siffror = [1, 1, 2, 3, 4]
 #[1, 2, 3, 4]
 
 print hand_siffror
 
-'''
-def par():
-    hand_siffror.sort()
-    langd = len(hand_siffror)
-    for n in range(langd-1):
-        if hand_siffror[n] == hand_siffror[n+1]:
-            return True
-    return False
-    if hand_siffror[0] == hand_siffror[1]:
-        return True
-    elif hand_siffror[1] == hand_siffror[2]:
-        return True
-    elif hand_siffror[2] == hand_siffror[3]:
-        return True
-    elif hand_siffror[3] == hand_siffror[4]:
-        return True
-    else:
-        return False
-print "par", par()
-'''
+
 
 #par i hand
 '''n = 0
@@ -70,16 +51,20 @@ def samlingar():
     for x in range(12):
         if hand_siffror.count(x+1) == 2:
             par_i_hand.append(x+1)
-
+'''
             print "Par i", (x+1)
     print par_i_hand
 print samlingar()
+'''
+#singelpar
 
 def par():
     if len(par_i_hand) == 1:
         return True
     else:
         return False
+
+#dubbelpar
 
 def tvapar():
     if len(par_i_hand) == 2:
@@ -88,25 +73,38 @@ def tvapar():
         return False
 
 
-
+#triss
 triss_i_hand=[]
+for x in range(12):
+    if hand_siffror.count(x+1) == 3:
+        triss_i_hand.append(x+1)
+
 def triss():
-    for x in range(12):
-        if hand_siffror.count(x+1) == 3:
-            triss_i_hand.append(x+1)
-            print "Triss i", x+1
-    print triss_i_hand
+    if len(triss_i_hand) == 3:
+        return True
+    else:
+        return False
 
-print triss()
 
+#fyrtal
+fyrtal_i_hand=[]
+for x in range(12):
+    if hand_siffror.count(x+1) == 4:
+        fyrtal_i_hand.append(x+1)
+
+def fyrtal():
+    if len(fyrtal_i_hand) == 4:
+        return True
+    else:
+        return False
 
 #full-house
 def full_house():
     if par() == True and triss() == True:
         return True
-print "du har full-house i", triss_i_hand, "och", par_i_hand
-
-
+    else:
+        return False
+#print "du har full-house i", triss_i_hand, "och", par_i_hand
 
 
 #farg i hand
@@ -116,9 +114,6 @@ def farg():
     else:
         return False
 
-
-
-
 #stege i hand
 def stege():
     hand_siffror.sort()
@@ -127,33 +122,3 @@ def stege():
         return True
     else:
         return False
-
-
-print stege()
-#farg ochs stege ar inte sa svart att fa till, men fragan ar om man ska gora det mer generellt? Men att hitta par i en hand med 10 kort kanske kan vara relevant, samma sak du letar efter.
-# men om vi letar farg i en hand med 10 kort, letar vi da efter 5 av samma farg eller hela handen(10 kort) i samma farg. Generellt i all ara, men om vi inte ens har definerat vad vi vill
-# ha blir det ju lite markligt att skriva en generell funktion pa det. Eftersom du inte finns ett vanligt spel med 10 kort pa hand dar nagot heter farg. Min tanke!
-
-
-
-
-
-
-
-
-
-'''
-def fyrtal():
-    hand_siffror.sort()
-    for n in range(4):
-        if hand_siffror[(n-1)] == hand_siffror[n] and hand_siffror[n] == hand_siffror[(n+1)] and hand_siffror[(n+1)] == hand_siffror[(n+2)]:
-            return True
-
-print fyrtal()
-
-for i in hand_siffror:
-    if hand_siffror[i] > 1:
-        print "you have pair"
-
-
-'''
